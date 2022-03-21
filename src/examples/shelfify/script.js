b339efd0dd5d199d6b9f2ac39a6979016c6cf2ab
mephistophelez <97410681+mephistophelez@users.noreply.github.com>
2022-03-21 11:48:45 +0100

    run

--- src/examples/shelfify/script.js
+++ src/examples/shelfify/script.js
@@ -9,7 +9,7 @@ loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
 // initialise 'data' object that will be used by compute()
 const data = {
-  definition: 'shelf-unroll.gh',
+  definition: 'shelf-final.gh',
   inputs: getInputs()
 }
 
